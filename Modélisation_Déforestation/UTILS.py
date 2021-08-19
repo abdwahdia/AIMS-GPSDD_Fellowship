57805e8d408f9449bddd00fd092647b693839d95
abdwahdia <55918904+abdwahdia@users.noreply.github.com>
2021-08-19 15:24:17 +0000

    Delete Modélisation_Déforestation directory

--- Modélisation_Déforestation/UTILS.py
+++
@@ -1,4 +0,0 @@
-def CRW(data,Data):
-    Array = list(data['Date'])
-    dat = Data.loc[Data['Date'].isin(Array)]
-    return dat
--- Modélisation_Déforestation/utils.py
+++
@@ -1,6 +0,0 @@
-def convert_dtype(data):
-    data[['Date']] = data[['Date']].astype(str).astype(int)
-    data[['Zone Perdue']] = data[['Zone Perdue']].astype(str).astype(float)
-    return data
-    
-    
